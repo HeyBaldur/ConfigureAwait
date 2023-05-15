3aa5aa9e98fe074338ad771c25b1b701b4ad015a
HeyBaldur ᛒᚨᛚᛞᚢᚱ <58003585+HeyBaldur@users.noreply.github.com>
2023-05-15 18:23:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ConfigureAwait.Tasks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConfigureAwait
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "MainThread";
+
+            Console.WriteLine($"Main running on thread {Thread.CurrentThread.Name}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                /// When you use the await keyword in C#, the code after it is executed 
+                /// on a different thread from the one that called it.
+                var result = await OperationService.SumAsyncWithConfigureAwait(i, i + 1);
+                Console.WriteLine($"the result is: {result}");
+            }
+        }
+    }
+}
---
+++ Tasks/OperationService.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading;
+using System;
+using System.Threading.Tasks;
+
+namespace ConfigureAwait.Tasks
+{
+    public class OperationService
+    {
+        /// <summary>
+        /// Create a sum async using ConfigureAwait
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static async Task<int> SumAsyncWithConfigureAwait(int a, int b)
+        {
+            /// ConfigureAwait(false) can be used to tell the await keyword 
+            /// that it should not resume execution on the original synchronization context 
+            /// when the code after await completes. This can improve performance and can help 
+            /// avoid deadlocks and other issues.
+            Console.WriteLine($"SumAsync running on thread {Thread.CurrentThread.Name}");
+
+            int sum = await Task.Run(() =>
+            {
+                if (Thread.CurrentThread.Name == null)
+                {
+                    Thread.CurrentThread.Name = Guid.NewGuid().ToString();
+                }
+
+                /// It's important to note that using ConfigureAwait(false) can have 
+                /// implications for how your code behaves, especially if you're working 
+                /// with user interfaces or other contexts that have specific synchronization 
+                /// requirements. In general, it's a good practice to use ConfigureAwait(false) 
+                /// when you're working with background tasks that don't require the original 
+                /// synchronization context.
+                Console.WriteLine($"Task.Run running on thread id: {Thread.CurrentThread.Name}");
+                return a + b;
+            }).ConfigureAwait(false);
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Create a sum async without ConfigureAwait
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static async Task<int> SumAsync(int a, int b)
+        {
+            Console.WriteLine($"SumAsync running on thread {Thread.CurrentThread.Name}");
+
+            int sum = await Task.Run(() =>
+            {
+                if (Thread.CurrentThread.Name == null)
+                {
+                    Thread.CurrentThread.Name = Guid.NewGuid().ToString();
+                }
+
+                Console.WriteLine($"Task.Run running on thread {Thread.CurrentThread.Name}");
+                return a + b;
+            });
+
+            return sum;
+        }
+    }
+}
